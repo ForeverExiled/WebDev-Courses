641417a1d90dcdb2397f5402f0854e6a29d66496
ForeverExiled <foreverexiled23@gmail.com>
2024-05-15 16:58:38 +0500

    HTML Course topic 6: Attributes

--- HTMLCourse/scripts/lightswitch.js
+++ HTMLCourse/scripts/lightswitch.js
@@ -1,3 +1,9 @@
 document.getElementById('switch').addEventListener('click', function() {
     document.body.classList.toggle('black');
 });
+
+// all custom attributes of any element are accessable with JS
+const el = document.querySelector('blockquote');
+for (const key in el.dataset) {
+    console.log(el.dataset[key]);
+}
